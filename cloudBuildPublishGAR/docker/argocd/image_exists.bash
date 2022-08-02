4def0d7c49efd45274740669daf356e2bd59268a
Christopher Maahs <cmaahs@gmail.com>
2022-08-02 06:51:39 -0500

    Check if the image exists, bail if so

---
+++ cloudBuildPublishGAR/docker/argocd/image_exists.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+IMAGE_REF=${LOCATION}-docker.pkg.dev/$PROJECT_ID/${REPOSITORY}/${IMAGE}:$(cat /workspace/IMAGE_TAG.txt)
+EXISTS=$(gcloud artifacts docker images describe ${IMAGE_REF} --format json | jq -r '.image_summary.repository')
+if [[ "${EXISTS}" == "docker" ]]; then
+  echo "The image tag ${IMAGE_REF} already exists in the Artifact Registry, please update your CHANGELOG.md"
+  exit 1
+fi
+
